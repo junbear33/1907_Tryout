427ca52e2d8588c59078a09de0fb3e05d16ec812
junbear33 <53137098+junbear33@users.noreply.github.com>
2019-07-28 15:52:26 +0900

    Update function.js

--- function.js
+++ function.js
@@ -3,11 +3,13 @@
 
     <script>
       function onInit(){
-        alert("数字を入力してください");
+        document.form.user_input.massage.value = "数字を入力してください";
+        document.form.user_input.input_data.value = "";
+        document.form.user_input.result.value = "";
       }
 
       function onSubmit(){
-        alert("計算します！");
+        document.form.user_input.massage.value = "計算します";
       }
 
   </script>
